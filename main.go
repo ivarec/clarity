ed29126602294e3d8f870483966f77f602658f6f
Ivan Reche <ivan.reche@gmail.com>
2024-08-10 20:09:37 -0300

    9p hello world (ramdisk)

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/knusbaum/go9p"
+	"github.com/knusbaum/go9p/fs"
+)
+
+func main() {
+	fs, _ := fs.NewFS("glenda", "glenda", 0777,
+		fs.WithCreateFile(fs.CreateStaticFile),
+		fs.WithCreateDir(fs.CreateStaticDir),
+		fs.WithRemoveFile(fs.RMFile),
+	)
+	// Listen on port 9999
+	go9p.Serve("0.0.0.0:9999", fs.Server())
+}
